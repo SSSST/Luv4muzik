b131ab774c2c20695bcba7719a1e58d93ecc70f0
SSSST <sssting82@gmail.com>
2018-08-24 09:27:29 +0800

    用户

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('brief');//简介
+            $table->boolean('is_musician')->default(false);//是否为音乐人
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+});//主页
+
+//用户相关
+{
+    Auth::routes();//用户注册、登录、修改密码
+    Route::get('users/{user}', 'UsersController@show')->name('users.show');//用户主页
+}
